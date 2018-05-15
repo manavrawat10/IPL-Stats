5a5d1c991d1de8fe57d8f4ea33001c2ab871706d
Manvenddra Singh Rawat <manavrawat10@gmail.com>
2018-05-16 00:07:34 +0530

    Update batting_graphs_controller.py

--- Batting/batting_graphs_controller.py
+++ Batting/batting_graphs_controller.py
@@ -6,6 +6,10 @@ Created on Sun May 13 18:47:21 2018
 """
 
 from Batting.most_runs import showMostRuns
+from Batting.most_runs_over import showMostRunsOver
     
 def callBattingGraphs(data, value):
-    showMostRuns(data)
+    if value==0:
+        showMostRuns(data)
+    elif value==1:
+        showMostRunsOver(data)
