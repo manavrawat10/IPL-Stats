560243a0921768934bd149bf2a1b59aa7df78749
Manvenddra Singh Rawat <manavrawat10@gmail.com>
2018-05-15 09:30:08 +0530

    Create bowling_graphs_controller.py

---
+++ Bowling/bowling_graphs_controller.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 13 18:47:21 2018
+
+@author: Manav
+"""
+
+from Bowling.most_wickets import showMostWickets
+
+def callBowlingGraphs(data, value):
+    showMostWickets(data)
