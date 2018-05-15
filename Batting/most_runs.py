d298150bca72d7e1e98d44fc6dab958f62c4bc7d
Manvenddra Singh Rawat <manavrawat10@gmail.com>
2018-05-15 22:28:18 +0530

    Update most_runs.py

--- Batting/most_runs.py
+++ Batting/most_runs.py
@@ -20,7 +20,25 @@ def showMostRuns(data):
     data = refineData(data)
     df = pd.DataFrame(data,columns=['Pos','Player','Mat','Inn', 'No','Runs','HS','AVG','BF','SR','100','50','4s','6s'],dtype=float)
     print(df)
-    barGraph(df)
+    stackBarGraph(df)
+    pieGraph(df)
+
+def stackBarGraph(df):
+    N = 10
+    ind = np.arange(N)    # the x locations for the groups
+    width = 0.9     # the width of the bars: can also be len(x) sequence
+
+    p1 = plt.bar(ind, df['Runs'], width)
+    p2 = plt.bar(ind, df['BF'], width,
+             bottom=df['Runs'])
+
+    plt.ylabel('Runs & Balls')
+    plt.title('Runs by Balls Faced')
+    plt.xticks(ind, df['Player'])
+    plt.yticks(np.arange(0, 81, 10))
+    plt.legend((p1[0], p2[0]), ('Runs', 'Balls Faced'))
+
+    plt.show()
 
 def barGraph(df):
     y_pos=np.arange(10)
@@ -32,4 +50,12 @@ def barGraph(df):
     plt.ylabel('Runs')
     plt.show()
 
-    
+def pieGraph(df):
+    colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+    explode = (0.1, 0, 0, 0,0,0,0,0,0,0)  # explode 1st slice
+        # Plot
+    plt.pie(df['Runs'], explode=explode, labels=(df['Player']), colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+ 
+    plt.axis('equal')
+    plt.show()
