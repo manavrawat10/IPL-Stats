5263e8aeab6ff89a2d5f3d6874e93be0eab0905b
Manvenddra Singh Rawat <manavrawat10@gmail.com>
2018-05-15 09:27:56 +0530

    Create batting_graphs_controller.py

---
+++ Batting/batting_graphs_controller.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 13 18:47:21 2018
+
+@author: Manav
+"""
+
+from Batting.most_runs import showMostRuns
+    
+def callBattingGraphs(data, value):
+    showMostRuns(data)
